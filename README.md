6b6663dc607d00635bf0fe5b785c7457974cde32
Yueyi Li <yueyili@Yueyis-MacBook-Pro.local>
2019-12-12 23:28:50 -0500

    readme unfinished

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
-# nba_data_analysis
+# Predicting NBA standings  - Who will make the 2019-2020 NBA playoffs in the Western Conference?
 
-This is the repo for our final project.
+
+## Overview
+This is the final project for Advanced Data Science @ JHU. This project built a prediction model for 
+
+## Data
+The data folder contains our training and testing data scraped from BASKETBALL REFERENCE. 
+* game_stats_all.csv - the team statistics of the first 20 games from season 2015-16 to season 2019-20. 
+* team_stats_all.csv - the game results (W-L record and scores) for each of the remaining 62 games from season 2015-16 to 2018-19. 
+* pre_20_games.rds - results for the first 20 games for each team of season 2019-2020
+* post_62_games_pred.rds - predictions for the rest of season 2019-20
+
+## Exploratory Data Analysis
+The plot folder contains plots from our EDA. 
+* home_away.png - 
+* player_ws_plot.png - 
+* team_def_off_plot.png - 
+* team_rank_plot.png - 
+* team_win_plot.png - 
+* team_ws_plot.png - 
+* west_east.png
+
+## Function source code
+* EDA_visualize.R
+* data_integration.R
+* data_scraping.R
+* data_wrangling.R
+* feature_eng.R
+* model_build.R
+* simulation.R
+
+## Shinyapp
+https://kate-yueyi-li.shinyapps.io/nba_simulation_shiny/
+
+
+
+To get a quick overview of the project, checkout our website at https://advds71x.github.io/NBAproj/ and Youtube video at https://www.youtube.com/watch?v=nkDTnJeeuJs&feature=youtu.be
+
+## Contributors
+Kate Li (yli324@jhu.edu)
+Runzhe Li (rli51@jhmi.edu)
+Yifan Zhang (yzhan170@jhu.edu)
+Linda Zhou (lzhou54@jhu.edu)
 
--- nba_simulation_shiny/simulation_shinyapp.R
+++ nba_simulation_shiny/simulation_shinyapp.R
@@ -3,9 +3,22 @@ library(dplyr)
 library(here)
 library(DT)
 library(shinyjs)
-test_set_pred<-readRDS(("test_set_pred.rds"))
+library(ggplot2)
 
-#start_filename = (file.path('start_icon.jpg'))
+#read in schdule and model prediction
+pre_20_games<-readRDS("pre_20_games.rds")
+post_62_games_pred<-readRDS("post_62_games_pred.rds")
+
+#a function that turns a decimals into percentage display
+percent <- function(x, digits = 2, format = "f", ...) {
+  paste0(formatC(100 * x, format = format, digits = digits, ...), "%")
+}
+
+brks <- seq(0.5,1,by=0.05)
+r<-round(seq(250, 44, length.out = length(brks) + 1), 0)
+g<-round(seq(250, 162, length.out = length(brks) + 1), 0)
+b<-round(seq(250, 95, length.out = length(brks) + 1), 0)
+clrs <- paste0("rgb(",r,",",g,",",b,")" )
 
 ui<-navbarPage("NBA Simulation",
            tabPanel("Game Simulation",
@@ -21,14 +34,14 @@ ui<-navbarPage("NBA Simulation",
                              tags$img(
                                src = 'start_icon.jpg',
                                       height = "200px"))
-                    
-                    )
+                           )
                     ),
+           
            tabPanel("Table Summary",
                     DTOutput("table")
                     ),
-           tabPanel("Play Offs",
-                    
+           
+           tabPanel("Playoffs",
                     fluidRow(
                       br(),
                         column(1,imageOutput("first"),offset =2),
@@ -42,9 +55,11 @@ ui<-navbarPage("NBA Simulation",
                         column(1,imageOutput("seventh"),offset=1),
                         column(1,imageOutput("eigth"),offset=1)
                         )
-              
-                    
-                    
+                    ),
+           
+           tabPanel("Games",
+                    dateInput("date","Date"),
+                    DTOutput("dailyTB")
                     )
 )
 
@@ -56,9 +71,9 @@ server <- function(input, output, session) {
   observeEvent(input$web_button,
                {
               hide("web_button")
-              test_set_pred$win<-rbinom(nrow(test_set_pred),1,test_set_pred[,8])
-               test_set_pred$winteam<-ifelse(test_set_pred$win==1,test_set_pred[,2],test_set_pred[,4])
-               test_set_pred$loseteam<-ifelse(test_set_pred$win==1,test_set_pred[,4],test_set_pred[,2])
+              post_62_games_pred$win<-rbinom(nrow(post_62_games_pred),1,post_62_games_pred[,8])
+               post_62_games_pred$winteam<-ifelse(post_62_games_pred$win==1,post_62_games_pred[,2],post_62_games_pred[,4])
+               post_62_games_pred$loseteam<-ifelse(post_62_games_pred$win==1,post_62_games_pred[,4],post_62_games_pred[,2])
                west_ind = data.frame(
                  western = c(0,1,1,0,0,0,0,0,1,1,0,1,1,1,0,0,0,0,1,1,1,1,1,1,0,0,0,0,1,1),
                  Team = c("Chicago Bulls","Los Angeles Lakers","Portland Trail Blazers", "Washington Wizards","Charlotte Hornets",
@@ -68,39 +83,42 @@ server <- function(input, output, session) {
                           "Sacramento Kings","Utah Jazz", "Minnesota Timberwolves","San Antonio Spurs","Cleveland Cavaliers",
                           "Toronto Raptors" ,"Boston Celtics","Philadelphia 76ers" ,"Golden State Warriors","Oklahoma City Thunder")
                )%>%filter(western==1)%>%select(Team)%>%mutate(win=rep(0,15),lose=rep(0,15))
+               
                ##calculate total win/lose 
-               for (j in (1:nrow(test_set_pred))){
+               for (j in (1:nrow(post_62_games_pred))){
                  for (i in west_ind$Team){
-                   if (test_set_pred[j,11]==i){
+                   if (post_62_games_pred[j,11]==i){
                      west_ind[west_ind$Team==i,"win"]=west_ind[west_ind$Team==i,"win"]+1
                    }
-                   if (test_set_pred[j,12]==i){
+                   if (post_62_games_pred[j,12]==i){
                      west_ind[west_ind$Team==i,"lose"]=west_ind[west_ind$Team==i,"lose"]+1
                    }
                  }
                }
+               
+               
                #playoffs
+               
+               west_ind$win<-west_ind$win+pre_20_games$W
+               west_ind$lose<-west_ind$lose+pre_20_games$L
                playoff<-west_ind[order(west_ind$win,decreasing =T),]$Team[1:8]
-               
-               
-               
-                
+
                rv <- reactiveValues(lines = character(0), n = 0, win=0,winteam=character(0),home=character(0),away=character(0))
                
                observe({
                  
-                 invalidateLater(300, session)
+                 invalidateLater(400, session)
                  
                  isolate({
-                   if (rv$n >= nrow(test_set_pred)) {
+                   if (rv$n >= nrow(post_62_games_pred)) {
                      return()
                    } else {
                      rv$n <- rv$n + 1
-                     rv$home=test_set_pred[rv$n,2]
-                     rv$away=test_set_pred[rv$n,4]
-                     #rv$win<-rbinom(1,1,test_set_pred[rv$n,8])
-                     rv$winteam<-test_set_pred[rv$n,11]
-                     rv$loseteam<-test_set_pred[rv$n,12]
+                     rv$home=post_62_games_pred[rv$n,2]
+                     rv$away=post_62_games_pred[rv$n,4]
+                     #rv$win<-rbinom(1,1,post_62_games_pred[rv$n,8])
+                     rv$winteam<-post_62_games_pred[rv$n,11]
+                     rv$loseteam<-post_62_games_pred[rv$n,12]
                      
                      
                    }
@@ -126,11 +144,12 @@ server <- function(input, output, session) {
                
                output$win<-renderImage({
                  filename <- normalizePath(file.path('./www',paste(rv$winteam, '.png', sep='')))
-                 list(src = filename,width=350,height=350)
+                 list(src = filename,width=270,height=270)
                },deleteFile = FALSE)
                
                output$table<-renderDT(
-                 west_ind
+                 datatable(west_ind, options = list(pageLength = 20,dom = 't'))
+                 
                )
                
                output$first<-renderImage({
@@ -172,11 +191,36 @@ server <- function(input, output, session) {
                  filename <- normalizePath(file.path('./www',paste(playoff[8], '.png', sep='')))
                  list(src = filename,width=100,height=100)
                },deleteFile = FALSE)
-               
-               
-               })
-  
 
+               })
+               
+  
+               
+  
+               output$dailyTB<-renderDT(
+ 
+                #display daily game prediction
+                datatable(data=post_62_games_pred[post_62_games_pred$Date==(as.character(format(input$date,"%a, %b %d, %Y"))),c(2,4,8)]%>%
+                   mutate(Home_Win_Prob=percent(pred_win_prob,digits=0),pred_lose_prob=1-pred_win_prob,Away_Win_Prob=percent(1-pred_win_prob,digits=0))%>%
+                     select(Home,Away,Home_Win_Prob,Away_Win_Prob,pred_win_prob,pred_lose_prob), options=list(
+                       #searching = FALSE, 
+                       pageLength = 20,
+                       dom = 't',
+                     columnDefs = list(list(visible=FALSE, targets=c(5,6)))
+                     
+                     )
+                   )%>%
+                   formatStyle(
+                     columns = c("Home","Away"),
+                     valueColumns = c("pred_win_prob","pred_lose_prob"),
+                     backgroundColor = styleInterval(brks,
+                                                    clrs )
+                   )
+                
+                   
+                 
+               )
+  
 
 }
 
